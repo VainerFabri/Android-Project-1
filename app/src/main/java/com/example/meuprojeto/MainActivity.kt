c2e8b0049e1be69d7bd3455b18e36f1550b18f0a
vainer <vainer_fabri@hotmail.com>
2019-07-09 12:46:37 -0300

    randomizando numeros

--- app/src/main/java/com/example/meuprojeto/MainActivity.kt
+++ app/src/main/java/com/example/meuprojeto/MainActivity.kt
@@ -3,10 +3,22 @@ package com.example.meuprojeto
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+import android.view.View
+import android.widget.TextView
+import java.util.*
+
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sortearNumero(view: View){
+        var texto:TextView = findViewById(R.id.textoSorteio)
+        var numeroSorteado = Random().nextInt(11)
+
+        texto.setText("O numero sorteado e: $numeroSorteado")
     }
 }
